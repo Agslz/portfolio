85cbeb0416684d314727758a8838e635e56a85e1
Agustin <agsp3rs3o@gmail.com>
2023-01-27 15:59:24 -0300

    Adding scrollReveal to profile__data

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,7 +65,7 @@ const sr = ScrollReveal({
 sr.reveal('.profile__border')
 sr.reveal('.profile__name', {delay:500})
 sr.reveal('.profile__profession', {delay:600})
-sr.reveal('.profile__social', {delay:700})
+sr.reveal('.profile__data', {delay:700})
 sr.reveal('.profile__info-group', {interval:100, delay:700})
 sr.reveal('.profile__buttons', {delay:800})
 sr.reveal('.profile__content', {delay:900})
